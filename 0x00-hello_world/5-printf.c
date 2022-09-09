6ea92703440362e4b8c09fd19ec8421f8fd51960
adetle <adetle022003@yahoo.com>
2022-09-08 19:03:10 -0700

    print f function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Printing the string function */
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf(str); /* writing the string to standard output*/
+	return (0);
+}
