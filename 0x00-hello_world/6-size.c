603cd2256493d7618996cbe09cb3c4cd1f2b5355
adetle <adetle022003@yahoo.com>
2022-09-08 20:08:52 -0700

    the c-hello introduction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	/* Printing the string function */
 	printf("Size of a char: %i byte(s)\n", (int) sizeof(chr));
-	printf("Size of an int: %i byte(s)\n", (int) sizeof(int))
+	printf("Size of an int: %i byte(s)\n", (int) sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", (int) sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", (int) sizeof(long long int));
     	printf("Size of a float: %i byte(s)\n", (int) sizeof(float));
