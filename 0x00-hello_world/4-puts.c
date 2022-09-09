85403b38a3c09d4b3deac63966c8c71f3010a9db
adetle <adetle022003@yahoo.com>
2022-09-08 18:08:26 -0700

    using puts to comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,10 @@
 #include <stdio.h>
-#include <string.h>
+int puts(const char* Str);
+int main(void)
+{
+	/* Printing the string function */
+	char Comment[] = "Programming is like building a multilingual puzzle"
 
+	puts(Comment); /* writing the string to standard output*/
+	return (0);
+}
