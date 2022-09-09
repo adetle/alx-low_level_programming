904e7265362403ed3d23df2beaafc74e35a80021
adetle <adetle022003@yahoo.com>
2022-09-08 18:21:15 -0700

    using puts to comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,14 +4,14 @@
 *
 * Description: Function to display comment using put
 *
-* Return: 0;
+* Return: (0);
 *
 */
 int main(void)
 {
 	/* Printing the string function */
-	char Comment[] = "Programming is like building a multilingual puzzle"
+	char Comt[] = "Programming is like building a multilingual puzzle"
 
-	puts(Comment); /* writing the string to standard output*/
+	puts(Comt); /* writing the string to standard output*/
 	return (0);
 }
