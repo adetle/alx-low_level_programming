46336654b94d67bb17623658bfaba59f415e03e2
adetle <adetle022003@yahoo.com>
2022-09-10 12:34:19 -0700

    digit combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+**/
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + x);
+		putchar(',');
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
