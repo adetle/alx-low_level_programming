d0c6a4e4786b4d2ab4fdad4567623de836908ea2
adetle <adetle022003@yahoo.com>
2022-09-10 00:58:52 -0700

    print alpabbbt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,8 +10,10 @@ int main(void)
 {
 	char x;
 
-	for (x = 'a'; x <= 'z' && x != q || e; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
+		if (x == q || x == e)
+		{ continue }
 		putchar(x);
 	}
 	putchar('\n');
