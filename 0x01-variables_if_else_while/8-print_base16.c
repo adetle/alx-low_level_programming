32324c65ec9cd5b276e674657f007017426953f3
adetle <adetle022003@yahoo.com>
2022-09-10 05:31:47 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+**/
+int main(void)
+{
+	unsigned char x = '0';
+		int i;
+	for (i = 0; i < 10; i++)
+	{
+		putchar(a);
+		a++;
+	}	
+		x = '1';
+		for (i = 0; i < 6; i++)
+	{
+		putchar('0' + x);
+		a++;								
+	}
+	putchar('\n');
+	return (0);
+}
