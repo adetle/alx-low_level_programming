a942539b33c72334098ce68ebf8c57db5861000b
adetle <adetle022003@yahoo.com>
2022-09-10 02:56:07 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+**/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar("%x", x);
+	}
+	putchar("\n");
+	return (0);
+}
