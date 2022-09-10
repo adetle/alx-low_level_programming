b3975a965e62f2ac3a61db285dae77953386f6ff
adetle <adetle022003@yahoo.com>
2022-09-10 01:44:22 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+**/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf(x\n);
+	}
+	return (0);
+}
