e4339ed24ecdd18f793473b7527d1656a8c2882d
adetle <adetle022003@yahoo.com>
2022-09-10 00:34:09 -0700

    print alpabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+**/
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z' && x != q || e; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
