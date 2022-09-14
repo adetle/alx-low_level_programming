a7e342f6d19e8de3ba92f9d14bfd322bed18d57d
adetle <adetle022003@yahoo.com>
2022-09-13 21:18:11 -0700

    alphabbet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,12 @@ void print_alphabet_x10(void)
 {
 	int i = 'a';
 	int j;
-	
 	for (j=0; j < 10; j++)
 	{
 		for (; i <= 'z'; i++)
 		{
 			_putchar(i);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
