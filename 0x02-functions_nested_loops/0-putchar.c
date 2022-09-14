dd4b93a2b9d3bc3b9da79eb15678d51832535694
adetle <adetle022003@yahoo.com>
2022-09-13 19:52:35 -0700

    function purchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main entry - Putchar
+ *
+ * Return: Always return 0;
+ */
+
+int main(void)
+{
+	putchar('-');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('a');
+	putchar('r');
+	return (0);
+}
