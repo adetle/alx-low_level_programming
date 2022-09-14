97fba0c8e5c30498f8f3a33c9fd4c15497f670fc
adetle <adetle022003@yahoo.com>
2022-09-13 23:00:07 -0700

    isalphabbet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * isalpha - Entry point
+ *
+ * @c: character to check for letter
+ *
+ * Return: a or A;
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z') || (c >='A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -33,4 +33,13 @@ void print_alphabet_x10(void);
  */
 
 int _islower(int c);
+
+/**
+ * int _isalpha(int c);
+ *
+ * return: 1 0r 0;
+ */
+
+int _isalpha(int c);
+
 #endif
