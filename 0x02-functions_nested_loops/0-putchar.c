7a5297a3dc1b74f413660cff363d7ea49372c259
adetle <adetle022003@yahoo.com>
2022-09-13 20:36:30 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,12 @@
 #include "main.h"
-
 /**
  * main - Entry point
  *
  * Return: Always return 0;
  */
-
 int main(void)
 {
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
