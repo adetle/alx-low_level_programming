1b626dbc8fc30d9d4da5f6186f940dfe4bdcb63e
adetle <adetle022003@yahoo.com>
2022-09-14 21:39:31 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -30,7 +30,7 @@ void times_table(void)
 				_putchar(z + '0');
 			} else
 			{
-				_putchar(' ');
+				_putchar(',');
 				_putchar(' ');
 				_putchar((z / 10) + '0');
 				_putchar((z % 10) + '0');
