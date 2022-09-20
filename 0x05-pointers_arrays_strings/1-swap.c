2987620680a16280ba2b7f150fcff9e6d6bb098d
adetle <adetle022003@yahoo.com>
2022-09-19 20:24:10 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+nclude "main.h"
+
+/**
+ * swap_int - Entry
+ * @a: first swap int
+ * @b: second swap int
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
