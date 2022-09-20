34edf82b76856562dbb3613c6018e0557dc25e3f
adetle <adetle022003@yahoo.com>
2022-09-19 20:37:19 -0700

    update main

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - Entry.
+ * @x: string to tbe reserved.
+ */
+void print_rev(char *x)
+{
+	int len = 0, i = 0;
+
+	while (x[i++])
+	{
+		len++;
+	}
+	
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(x[i]);
+	}
+
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,12 +2,11 @@
 #define MAIN_H
 #include <stdlib.h>
 
-int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 size_t _strlen(const char *str);
 void _puts(char *str);
-void print_rev(char *s);
+void print_rev(char *x);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
