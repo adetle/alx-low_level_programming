cf5c0bfdb7d9fd4dee603c05846115a61265eac7
adetle <adetle022003@yahoo.com>
2022-09-27 15:55:56 -0700

    update

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _strchr - find a character in a string
+* @s: string to check
+* @c: check against
+*
+* Return: number of character in a string
+*/
+char *_strchr(char *s, char c)
+{
+int i;
+
+for (i = 0; *(s + i); i++)
+{
+if (*(s + i) == c)
+return (s + i);
+}
+if (*(s + i) == c)
+return (s + i);
+return (0);
+}
+Footer
+
