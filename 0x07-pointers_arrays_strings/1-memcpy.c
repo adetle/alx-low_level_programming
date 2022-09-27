f35090d01512d017570dc70f595f30e9e70e449d
adetle <adetle022003@yahoo.com>
2022-09-27 15:34:00 -0700

    mmory copy of byte

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memcpy - this copy byte from  memory area to another
+ * @*dest: destination area
+ * @*src: source area
+ * @n: number of byte to copy
+ *
+ * Return: Always 0
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+	unsigned int n;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+	return (dest);
