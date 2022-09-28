511f89f90a87ce3858de1bf3d301fa3e7c56ed2f
adetle <adetle022003@yahoo.com>
2022-09-27 17:03:11 -0700

    crackme password

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - setting value of a pointer to char
+ * @s: pointer to set
+ * @to: value to set
+ *
+ * Return: Always 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
