d4860c7fdb2e9aa620fb1b4447b0c040403abb2a
adetle <adetle022003@yahoo.com>
2022-09-28 01:02:18 -0700

    put_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that print a string
+ * @s: string to be printing
+ *
+ * Return: Always 0
+ */
+
+void _puts_recursion(char *s)
+{
+	char s;
+
+	_putchar('s', \n)
+	return 0;
+}
