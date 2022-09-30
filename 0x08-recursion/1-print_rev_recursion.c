aa93522abf104aa1175731545fbdc4d36d99b869
adetle <adetle022003@yahoo.com>
2022-09-29 18:38:47 -0700

    putchar corrected

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print string in reverse
+ *
+ * @s: target string
+ *
+ * Return: Always 0
+ */
+
+viod _printOrev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s)
+}
