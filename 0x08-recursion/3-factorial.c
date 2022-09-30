aa22def79348ba1e6221ab51f800e0f30ea6321a
adetle <adetle022003@yahoo.com>
2022-09-29 19:06:28 -0700

    3-factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial -> function factorial
+ * @n: input number
+ * Return: an integer
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	return (n * factorial(n - 1));
+}
